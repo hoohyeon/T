6e4d88735774c4dee9878f71eee0efbb015c884a
hoohyeon <156387278+hoohyeon@users.noreply.github.com>
2025-03-04 17:28:03 +0900

    Title: DateTime to DateTime, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 250304/DateTime to DateTime/datetime-to-datetime.py
+++ 250304/DateTime to DateTime/datetime-to-datetime.py
@@ -3,7 +3,7 @@ a, b, c = map(int, input().split())
 start = 24 * 60 * 11 + 60 * 11 + 11
 end = 24 * 60 * a + 60 * b + c
 
-if end - start > 0:
+if end - start >= 0:
     print(end-start)
 else:
     print(-1)
