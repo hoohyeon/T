65f9a1e648561552d5f02dddd2dc4d0ce343e879
hoohyeon <156387278+hoohyeon@users.noreply.github.com>
2025-03-07 13:23:13 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 94ms, Memory: 48MB, Status: Passed - CodeTree

--- 250307/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 250307/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -4,30 +4,26 @@ offset = 1000
 
 points = [[0] * 2000 for _ in range(2000)]
 
-# 첫 번째 사각형을 1로 설정
 for i in range(x1 + offset, x2 + offset):
     for j in range(y1 + offset, y2 + offset):
         points[i][j] = 1
 
-# 두 번째 사각형을 0으로 덮음
 for i in range(x3 + offset, x4 + offset):
     for j in range(y3 + offset, y4 + offset):
         points[i][j] = 0
 
-# 최소/최대 x, y 좌표 찾기
 min_x, max_x = 2000, -2000
 min_y, max_y = 2000, -2000
 
 for i in range(x1 + offset, x2 + offset):
     for j in range(y1 + offset, y2 + offset):
         if points[i][j] == 1:
-            min_x = min(min_x, i)
-            max_x = max(max_x, i)
-            min_y = min(min_y, j)
-            max_y = max(max_y, j)
+            min_x = min(min_x, j)
+            max_x = max(max_x, j)
+            min_y = min(min_y, i)
+            max_y = max(max_y, i)
 
-# 사각형이 완전히 지워졌다면 0 출력
-if min_x == float('inf'):
+if min_x == 2000:
     print(0)
 else:
     print((max_x - min_x + 1) * (max_y - min_y + 1))
