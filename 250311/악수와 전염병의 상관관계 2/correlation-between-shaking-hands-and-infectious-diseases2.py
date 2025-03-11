d236fc9827c57f597795db18e8552af00da29f8e
hoohyeon <156387278+hoohyeon@users.noreply.github.com>
2025-03-11 09:53:34 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250311/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 250311/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -9,17 +9,15 @@ infected[P] = 1         # 감염 시작
 handshakes.sort()
 
 for t, x, y in handshakes:
-    if infected[x] and infected[y]:
+    if infected[x]:
         count[x] += 1
-        count[y] += 1
+        if count[x] < K:
+            infected[y] = 1
 
-    elif infected[x] and count[x] < K:
-        infected[y] = 1
-        count[x] += 1
-
-    elif infected[y] and count[y] < K:
-        infected[x] = 1
+    if infected[y]:
         count[y] += 1
-    
+        if count[y] < K:
+            infected[x] = 1
+            
 for person in infected[1:]:
     print(person, end='')
