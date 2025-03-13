ec5fec346bd422b6b8e48282d1310df1b43ab71e
hoohyeon <156387278+hoohyeon@users.noreply.github.com>
2025-03-13 23:51:42 +0900

    Title: 인접하지 않은 2개의 수, Time: 48ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250313/인접하지 않은 2개의 수/two-non-adjacent-numbers.py
@@ -0,0 +1,12 @@
+n = int(input())
+numbers = list(map(int, input().split()))
+
+max_sum = 0
+
+for i in range(n):
+    for j in range(i+2, n):
+        sum = numbers[i] + numbers[j]
+        
+        max_sum = max(max_sum, sum)
+
+print(max_sum)
