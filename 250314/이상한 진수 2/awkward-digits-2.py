bf5019b80db27b4b44d389b3a60559d4121d8cc2
hoohyeon <156387278+hoohyeon@users.noreply.github.com>
2025-03-14 10:40:31 +0900

    Title: 이상한 진수 2, Time: 50ms, Memory: 16MB, Status: Passed - CodeTree

--- 250314/이상한 진수 2/awkward-digits-2.py
+++ 250314/이상한 진수 2/awkward-digits-2.py
@@ -1,8 +1,12 @@
 a = input()
 
-for i in range(len(a)):
-    if a[i] == '0':
-        a = a[:i] + '1' + a[i+1:]
-        break
+if '0' in a:
+    for i in range(len(a)):
+        if a[i] == '0':
+            a = a[:i] + '1' + a[i+1:]
+            break
 
+else:
+    a = a[:-1] + '0'
+    
 print(int(a, 2))
