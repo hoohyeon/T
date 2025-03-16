473e6b7fd3abf6dafa3269ff8806510fa93dde22
hoohyeon <156387278+hoohyeon@users.noreply.github.com>
2025-03-16 23:33:04 +0900

    Title: 두 선분, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

--- 250316/두 선분/two-lines.py
+++ 250316/두 선분/two-lines.py
@@ -1,10 +1,6 @@
 x1, x2, x3, x4 = map(int, input().split())
 
-if x3 <= x1 <= x4:
+if max(x1, x3) <= min(x2, x4):
     print('intersecting')
-
-elif x3 <= x2 <= x4:
-    print('intersecting')
-
 else:
     print('nonintersecting')
