79114fed310b5f3138ba6591b17af32f5dc6808a
hoohyeon <156387278+hoohyeon@users.noreply.github.com>
2025-03-17 13:22:56 +0900

    Title: 되돌아오기, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250317/되돌아오기/come-back.py
+++ 250317/되돌아오기/come-back.py
@@ -1,26 +1,27 @@
-N = int(input())
-moves = [tuple(input().split()) for _ in range(N)]
+n = int(input())
+moves = [tuple(input().split()) for _ in range(n)]
 dir = [move[0] for move in moves]
 dist = [int(move[1]) for move in moves]
 
-direction_map = {'N': (0, 1), 'S': (0, -1), 'E': (1, 0), 'W': (-1, 0)}
+direction_dict = {'W' : (-1, 0), 'S' : (0, -1), 'N' : (0, 1), 'E' : (1, 0)}
 
 x, y = 0, 0
 time = 0
-result = - 1
+result = -1
+check = False
 
-for i in range(N):
-    dx, dy = direction_map[dir[i]]
+for i in range(n):
+    dx, dy = direction_dict[dir[i]]
 
     for _ in range(dist[i]):
+
         x += dx
         y += dy
         time += 1
 
-        if x == 0 and y == 0:
+        if x == 0 and y == 0 and not check:
             result = time
+            check = True
+            break
 
-    if result != -1:
-        break
-    
 print(result)
