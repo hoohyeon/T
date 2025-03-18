663033ff1c2a5b5eccbb4e2130056780613db8ae
hoohyeon <156387278+hoohyeon@users.noreply.github.com>
2025-03-18 14:23:51 +0900

    Title: 거울에 레이저 쏘기 2, Time: 54ms, Memory: 21MB, Status: Wrong Answer - CodeTree

---
+++ 250318/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -0,0 +1,45 @@
+n = int(input())
+mirror = [list(input()) for _ in range(n)]
+k = int(input())
+razer = [[0] * (n+1) for _ in range(n+1)]
+
+dir = k // n
+dx, dy = [0, -1, 0, 1], [-1, 0, 1, 0]
+cnt = 0
+
+if dir == 0:
+    x, y = (1, k)
+elif dir == 1:
+    x, y = (k-n, k)
+elif dir == 2:
+    x, y = (n, 3*n-k+1)
+elif dir == 3:
+    x, y = (4*n-k+1, 1)
+
+while 0 < x < n and 0 < y < n:
+    if mirror[x-1][y-1] == '/':
+        if dir == 0:
+            dir = 1
+        elif dir == 1:
+            dir = 0
+        elif dir == 2:
+            dir = 3
+        elif dir == 3:
+            dir = 2
+
+    elif mirror[x-1][y-1] == '\\':
+        if dir == 0:
+            dir = 3
+        elif dir == 1:
+            dir = 2
+        elif dir == 2:
+            dir = 1
+        elif dir == 3:
+            dir = 0
+
+    x += dx[dir]
+    y += dy[dir]
+    
+    cnt += 1
+
+print(cnt)
