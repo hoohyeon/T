3275cf306fa7b2d7b52f62d60405b2e6dbb7d08b
hoohyeon <156387278+hoohyeon@users.noreply.github.com>
2025-03-18 15:07:09 +0900

    Title: 거울에 레이저 쏘기 2, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250318/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
+++ 250318/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -2,44 +2,24 @@ n = int(input())
 mirror = [list(input()) for _ in range(n)]
 k = int(input())
 
-razer = [[0] * (n+1) for _ in range(n+1)]
-
-
-dir = (k-1) // n
 dx = [1, 0, -1, 0]
 dy = [0, -1, 0, 1]
+
+dir = (k-1) // n
 cnt = 0
 
-if dir == 0:
-    x, y = (1, k)
-elif dir == 1:
-    x, y = (k-n, n)
-elif dir == 2:
-    x, y = (n, 3*n-k+1)
-elif dir == 3:
-    x, y = (4*n-k+1, 1)
+start_pos = [(1, k), (k-n, n), (n, 3*n-k+1), (4*n-k+1, 1)]
+x, y = start_pos[dir]
+
+mirror_change = {
+    '/': {0: 1, 1: 0, 2: 3, 3: 2},
+    '\\': {0: 3, 1: 2, 2: 1, 3: 0}
+}
 
 while 0 < x <= n and 0 < y <= n:
-    if mirror[x-1][y-1] == '/':
-        if dir == 0:
-            dir = 1
-        elif dir == 1:
-            dir = 0
-        elif dir == 2:
-            dir = 3
-        elif dir == 3:
-            dir = 2
-
-    elif mirror[x-1][y-1] == '\\':
-        if dir == 0:
-            dir = 3
-        elif dir == 1:
-            dir = 2
-        elif dir == 2:
-            dir = 1
-        elif dir == 3:
-            dir = 0
-
+    if mirror[x-1][y-1] in mirror_change:
+        dir = mirror_change[mirror[x-1][y-1]][dir]
+    
     x += dx[dir]
     y += dy[dir]
     
