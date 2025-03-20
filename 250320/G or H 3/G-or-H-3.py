d4d6f7ef44846e78bc21be57eeb0c78163fb6727
hoohyeon <156387278+hoohyeon@users.noreply.github.com>
2025-03-20 13:48:19 +0900

    Title: G or H 3, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250320/G or H 3/G-or-H-3.py
@@ -0,0 +1,19 @@
+n, k = map(int, input().split())
+arr = [0] * 10001
+max_sum = 0
+
+for _ in range(n):
+    p, a = input().split()
+    p = int(p)
+
+    if a == 'G':
+        arr[p] = 1
+    
+    elif a == 'H':
+        arr[p] = 2
+
+for i in range(n-k+1):
+    c_sum = sum(arr[i:i+k+2])
+    max_sum = max(max_sum, c_sum)
+
+print(max_sum)
