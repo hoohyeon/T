68c31ecd26d85dfc3fed238954ef4e6c401bd024
hoohyeon <156387278+hoohyeon@users.noreply.github.com>
2025-03-21 15:14:11 +0900

    Title: 바구니 안의 사탕 2, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

--- 250321/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 250321/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -8,7 +8,7 @@ for _ in range(n):
     basket[coor] += candy
 
 max_sum = 0
-for c in range(k, (100-k) + 1):
+for c in range(1, 100 + 1):
     c_sum = sum(basket[c-k:c+k+1])
 
     max_sum = max(max_sum, c_sum)
