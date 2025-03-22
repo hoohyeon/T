23c35188ada0bf9ca975d3fa21a1bf3b72918bc1
hoohyeon <156387278+hoohyeon@users.noreply.github.com>
2025-03-22 23:24:23 +0900

    Title: 개발자의 능력 3, Time: 51ms, Memory: 16MB, Status: Passed - CodeTree

--- 250322/개발자의 능력 3/ability-of-developer-3.py
+++ 250322/개발자의 능력 3/ability-of-developer-3.py
@@ -3,12 +3,12 @@ scores = list(map(int, input().split()))
 min_gap = sum(scores)
 
 for i in range(4):
-    for j in range(i, 5):
-        for k in range(j, 6):
+    for j in range(i+1, 5):
+        for k in range(j+1, 6):
             c_sum = scores[i] + scores[j] + scores[k]
 
             gap = abs(c_sum - (sum(scores) - c_sum))
 
-            min_gap = min(min_gap, gap) 
-
+            min_gap = min(min_gap, gap)
+            
 print(min_gap)
